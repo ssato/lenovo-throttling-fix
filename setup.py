10118a96858bc4652cfc6ee61714f5026c135590
Satoru SATOH <satoru.satoh@gmail.com>
2018-09-30 01:56:53 +0900

    enhancement: add setup.py and its config file for installation
    Add setup.py and its configuration file to make this package installable
    without install.sh. This enhancement should make it easier to package
    this a lot.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import
+import os.path
+from setuptools import setup
+
+
+PACKAGE = "lenovo_throttling_fix"
+
+DATA_FILES = [("etc", ["etc/lenovo_throttling_fix.conf"]),
+              ("usr/lib/systemd/system", ["systemd/lenovo_throttling_fix.service"])]
+
+
+setup(name=PACKAGE,
+      include_package_data=True,
+      data_files=DATA_FILES)
+
+# vim:sw=4:ts=4:et:
