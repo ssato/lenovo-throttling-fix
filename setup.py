e12c320fa0e1bfaceea04722a02e3917eb8df7b3
Satoru SATOH <satoru.satoh@gmail.com>
2018-10-15 16:25:44 +0900

    enhancement: override bdist_rpm to use custom RPM SPEC template
    override bdist_rpm in setup.py to use custom RPM SPEC template.

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import
+import os
 import os.path
+import setuptools.command.bdist_rpm
 from setuptools import setup
 
 
@@ -7,6 +9,15 @@ DATA_FILES = [("/etc", ["etc/lenovo_throttling_fix.conf"]),
               ("/usr/lib/systemd/system", ["systemd/lenovo_throttling_fix.service"])]
 
 
-setup(data_files=DATA_FILES)
+class bdist_rpm(setuptools.command.bdist_rpm.bdist_rpm):
+    """Override to use custom RPM SPEC template file.
+    """
+    def _make_spec_file(self):
+        return open(os.path.join(os.curdir,
+                    "lenovo-throttling-fix.spec")).readlines()
+
+
+setup(cmdclass=dict(bdist_rpm=bdist_rpm),
+      data_files=DATA_FILES)
 
 # vim:sw=4:ts=4:et:
