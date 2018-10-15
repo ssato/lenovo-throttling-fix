fec67ad31923bf55cf9f91ecc03bf5a6b41cf542
Satoru SATOH <satoru.satoh@gmail.com>
2018-10-15 16:13:15 +0900

    fix: remove 'name' argument from setup.py#setup()
    remove 'name' argument from setup.py#setup() because it's in setup.cfg.

--- setup.py
+++ setup.py
@@ -3,13 +3,10 @@ import os.path
 from setuptools import setup
 
 
-PACKAGE = "lenovo_throttling_fix"
-
 DATA_FILES = [("/etc", ["etc/lenovo_throttling_fix.conf"]),
               ("/usr/lib/systemd/system", ["systemd/lenovo_throttling_fix.service"])]
 
 
-setup(name=PACKAGE,
-      data_files=DATA_FILES)
+setup(data_files=DATA_FILES)
 
 # vim:sw=4:ts=4:et:
