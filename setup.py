0ad5a59c153f61831da25af800fa7a2bcb798d30
Satoru SATOH <satoru.satoh@gmail.com>
2018-10-15 16:28:04 +0900

    refactor: use fully qualified import

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from __future__ import absolute_import
 import os
 import os.path
+import setuptools
 import setuptools.command.bdist_rpm
-from setuptools import setup
 
 
 DATA_FILES = [("/etc", ["etc/lenovo_throttling_fix.conf"]),
@@ -17,7 +17,7 @@ class bdist_rpm(setuptools.command.bdist_rpm.bdist_rpm):
                     "lenovo-throttling-fix.spec")).readlines()
 
 
-setup(cmdclass=dict(bdist_rpm=bdist_rpm),
-      data_files=DATA_FILES)
+setuptools.setup(cmdclass=dict(bdist_rpm=bdist_rpm),
+                 data_files=DATA_FILES)
 
 # vim:sw=4:ts=4:et:
